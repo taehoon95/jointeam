0805c10e61768a74c71194f5a3bf659c20001a9d
taehoon95 <leeth950304@gmail.com>
2021-09-07 13:54:08 +0900

    태훈 수정

--- src/main/java/pack/UpdateServlet.java
+++ src/main/java/pack/UpdateServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/update.do")
+@WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
