ca14ec9b369646d2fc0a8d24c7e21e5fe87af337
flower-pg <cothd97@naver.com>
2021-09-07 13:55:46 +0900

    21.09.07(화) [ADD]회원가입 성공 페이지 완료

--- src/main/java/pack/RegisterServlet.java
+++ src/main/java/pack/RegisterServlet.java
@@ -1,41 +1,41 @@
 package pack;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteServlet
- */
-@WebServlet("/RegisterServlet")
+@WebServlet("/register") 
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public RegisterServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        System.out.println("생성자 콜");
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("doPost");
+		request.setCharacterEncoding("euc-kr");
+		String id = request.getParameter("id");
+		System.out.println(id);
+		
+		response.setContentType("text/html;charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+			out.println("<body>");
+				out.println("<h1>" + id + " 가입됨 </h1>");	
+			out.println("</body>");
+		out.println("</html>");
+		out.close();
 	}
 
 }
