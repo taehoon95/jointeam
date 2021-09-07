7f53576131e8f7f25b2049a768fbbcb513e78edf
taehoon95 <leeth950304@gmail.com>
2021-09-07 13:53:04 +0900

    태훈 회원 수정

--- src/main/java/pack/UpdateServlet.java
+++ src/main/java/pack/UpdateServlet.java
@@ -1,41 +1,42 @@
 package pack;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteServlet
- */
-@WebServlet("/UpdateServlet")
+@WebServlet("/update.do")
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public UpdateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    	System.out.println("수정 생성자 콜");
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("두갯");
+		Map<Integer,String> empMap = new HashMap<>();
+		response.setContentType("text/html; charset = utf-8");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println(
+				
+				"<html>"+
+						"<body>"+
+							"<h1>${name}수정 되었습니다.</h1>"+
+						"</body>"+
+				"</html>"
+		);
+		out.close();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
